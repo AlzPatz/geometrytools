8a08a0858e0185175148766437ea4c8d24c7d84a
Alex <alexpatersonsmail@gmail.com>
2018-07-31 21:09:04 +0100

    ... attempt #7

--- build.bat
+++ build.bat
@@ -12,6 +12,6 @@ if not "%PackageVersion%" == "" (
 %MsBuildExe% geometrytools\GeometryTools.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir geometrytools\bin
-mkdir geometrytools\bin\Release
+mkdir geometrytools\bin\%config%
 
 %NuGet% pack "geometrytools\geometrytools.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"

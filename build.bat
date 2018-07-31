526e5a747994d00330f5ac2732cd0dc088c1ebe7
Alex <alexpatersonsmail@gmail.com>
2018-07-31 21:30:58 +0100

    ... attempt #17

--- build.bat
+++ build.bat
@@ -15,4 +15,9 @@ mkdir geometrytools\bin\%config%\netstandard2.0
 
 %MsBuildExe% geometrytools\GeometryTools.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
+cd bin\%config%\netstandard2.0
+echo ls
+cd ..
+cd ..
+
 %NuGet% pack "geometrytools\geometrytools.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"

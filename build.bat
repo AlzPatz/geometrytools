530f42893ae02403939f29c9f84bec1949bb56c2
Alex <alexpatersonsmail@gmail.com>
2018-07-31 20:23:18 +0100

    Setting up MyGet automatic package building ... attempt #1 .. Good Luck

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+%MsBuildExe% geometrytools\GeometryTools.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+%NuGet% pack "geometrystools\geometrytools.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"

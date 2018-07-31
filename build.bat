0cbd93fd92ae5af26e33e46789719544e4dc743e
Alex <alexpatersonsmail@gmail.com>
2018-07-31 21:33:26 +0100

    ... attempt #18

--- build.bat
+++ build.bat
@@ -16,7 +16,8 @@ mkdir geometrytools\bin\%config%\netstandard2.0
 %MsBuildExe% geometrytools\GeometryTools.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 cd bin\%config%\netstandard2.0
-echo ls
+ls
+cd ..
 cd ..
 cd ..
 

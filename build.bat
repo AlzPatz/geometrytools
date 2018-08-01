8e3de3c6cd668be0ba6ad983477467e9e89d4366
Alex <alexpatersonsmail@gmail.com>
2018-08-01 06:58:35 +0100

    ... trying again to simplify the build.bat

--- build.bat
+++ build.bat
@@ -9,16 +9,10 @@ if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
 
-mkdir geometrytools\bin
-mkdir geometrytools\bin\%config%
-mkdir geometrytools\bin\%config%\netstandard2.0
-
 cd geometrytools
 
 dotnet build 
 
 cd ..
-
-dir geometrytools\bin\%config%\netstandard2.0
 
 %NuGet% pack "geometrytools\geometrytools.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
